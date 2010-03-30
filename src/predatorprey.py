10b55579bfa229d17458c0596bdbd2e43a538575
sbadame <s.badame@gmail.com>
2010-03-30 18:57:47 -0400

    a bunch of renames, added ai.py

--- src/predatorprey.py
+++ src/predatorprey.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 #Deniz put this crap in a loop
-import PredPreyAlgorithm
+import predpreyalgorithm
 
 generations = input("How many generations would you like this to run for?")
 num_of_preds_per_gen = input("How many predators would you like per generation?")
 num_of_prey_per_gen =  input("How many prey would you like per generation?")
 
-PredPreyAlgorithm.mutate(generations, num_of_preds_per_gen, num_of_prey_per_gen)
-print(PredPreyAlgorithm.best_pred)
-print(PredPreyAlgorithm.best_prey)
+predpreyalgorithm.mutate(generations, num_of_preds_per_gen, num_of_prey_per_gen)
+print(predpreyalgorithm.best_pred[0])
+print(predpreyalgorithm.best_prey[0])
