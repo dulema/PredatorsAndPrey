de9356d72d75867a8cdbaac66f9e1443c0113b3d
sbadame <s.badame@gmail.com>
2010-04-02 13:39:49 -0400

    I got map working...

--- src/predpreyalgorithm.py
+++ src/predpreyalgorithm.py
@@ -1,6 +1,6 @@
 #Does the work
 import random
-import map
+import predpreymap
 from multiprocessing import Pool
 
 mapsize = 23
@@ -8,12 +8,6 @@ plant = 0.5
 
 predcount = 3
 preycount = 10
-
-def createDefaultPredStatus():
-    return {"hunger":100, "sleepiness":100, "isalive":True}
-
-def createDefaultPreyStatus():
-    return {"hunger":100, "sleepiness":100, "isalive":True}
 
 def getDefaultPreds(num):
     for _ in range(num):
@@ -23,98 +17,98 @@ def getDefaultPreys(num):
     for _ in range(num):
 	yield createDefaultPreyStatus();
 
-best_pred = ({"hungervotes":1, "grouping":1, "fatigue":1}, createDefaultPredStatus())
-best_prey =({"hungervotes":1, "fearvotes":1, "grouping":1, "fatigue":1}, createDefaultPreyStatus()) 
+best_pred = {"hungervotes":1, "grouping":1, "fatigue":1}
+best_prey = {"hungervotes":1, "fearvotes":1, "grouping":1, "fatigue":1}
 
 def mutateBehavior(critter):
-	behavior = critter[0]
-	mutation = {}
-	for key in iter(behavior):
-		mutation[key] = behavior[key] + (random.randint(-3, 3))
-		while mutation[key] < 1:
-			mutation[key] = behavior[key] + (random.randint(-3, 3))
-	return mutation 
+    mutation = {}
+    for key in iter(critter):
+	    mutation[key] = critter[key] + (random.randint(-3, 3))
+	    while mutation[key] < 1:
+		    mutation[key] = critter[key] + (random.randint(-3, 3))
+    return mutation 
          
 def createPreyMutation(prey, number):
 	for i in range(number):
-		yield (mutateBehavior(prey), createDefaultPreyStatus()) 
+		yield mutateBehavior(prey) 
 
 def createPredatorMutation(predator, number):
 	for i in range(number):
-		yield (mutateBehavior(predator), createDefaultPredStatus())
+		yield mutateBehavior(predator)
 		
-def score(x):
-    	pred = x[0]
-    	prey = x[1]
-	return sum([pred[1][i] for i in iter(pred[1])])
+def score(pred, prey):
+    return sum( [pred[i] for i in iter(pred)] )
 
-'''
-    	global mapsize, plant, predcount, preycount
-    	map = map.Map(mapsize, plant)
 
-	preds = []
-	for i in range(predcount):
-	    tpred = (pred[0], createDefaultPredStatus())
-	    map.setCritterAt(map.getRandomUntakenTile(), tpred) 
-	    preds.append(tpred)
-
-	preys = []
-	for i in range(preycount):
-	    tprey = (prey[0], createDefaultPreyStatus())
-	    map.setCritterAt(map.getRandomUntakenTile(),tprey) 
-	    preys.append(tprey)
-
+#    	global mapsize, plant, predcount, preycount
+#    	map = map.Map(mapsize, plant)
+#
+#    	preds = []
+#	for i in range(predcount):
+#	    tpred = (pred[0], createDefaultPredStatus())
+#	    map.setCritterAt(map.getRandomUntakenTile(), tpred) 
+#	    preds.append(tpred)
+#
+#	preys = []
+#	for i in range(preycount):
+#	    prey = (prey[0], createDefaultPreyStatus())
+#	    map.setCritterAt(map.getRandomUntakenTile(),tprey) 
+#	    preys.append(tprey)
+#
 	#This is a totally horrible way to do this
-	days = 0
-	while len(preys) != 0 and len(preds) != 0:
-	    days = days + 1
-	    for p in preys:
-		map.moveCritter(p, ai.getPreyNextMove(map, p))
+#	days = 0
+#	while len(preys) != 0 and len(preds) != 0:
+#	    days = days + 1
+#	    for p in preys:
+#		map.moveCritter(p, ai.getPreyNextMove(map, p))
+#
+#	    for p in preds:
+#		map.moveCritter(p, ai.getPredatorNextMove(map, p))
+#	
+#	    preys = filter(lambda x : x[1]["isalive"], preys)
+#	    preds = filter(lambda x : x[1]["isalive"], preds)
+#
+#	return days 
 
-	    for p in preds:
-		map.moveCritter(p, ai.getPredatorNextMove(map, p))
-	
-	    preys = filter(lambda x : x[1]["isalive"], preys)
-	    preds = filter(lambda x : x[1]["isalive"], preds)
-
-	return days 
-
-'''
 
 def mutate(gens, num_of_preds_per_gen, num_of_prey_per_gen): 
     global best_pred, best_prey, score
-    for i in range(gens):
+    for _ in range(gens):
 
-	#Generate a list of all of the preds
 	preds = [pred for pred in createPredatorMutation(best_pred, num_of_preds_per_gen - 1)]
 	preds.append(best_pred)
- 
- 	#Generate a Pool of processes to run all of the scoring for the predators
-	predPool = Pool(processes=len(preds))
-	predResult = predPool.map_async(score, zip(preds, [best_prey for _ in range(len(preds))]) )
 
-	#Generate a Pool of processes to run all of the scoring for the prey
 	preys =  [prey for prey in createPreyMutation(best_prey, num_of_prey_per_gen - 1)]
 	preys.append(best_prey)
 
-	preyPool = Pool(processes=len(preys))
-	preyResult = preyPool.map_async(score, zip([best_pred for _ in range(len(preys))], preys ))
+ 
+#	Generate a Pool of processes to run all of the scoring for the predators
+#	predPool = Pool(processes=len(preds))
+#	predResult = predPool.map_async(score, zip(preds, [best_prey for _ in range(len(preds))]) )
+
+
+#	preyPool = Pool(processes=len(preys))
+#	preyResult = preyPool.map_async(score, zip([best_pred for _ in range(len(preys))], preys ))
 	
+
 	#Parse the results
-	predscores = predResult.get(None) #Probably dangerous to not specify a timeout
-	score = max(predscores)
-	index = predscores.index(score)
-	best_pred = predscores[index]
+#	predscores = predResult.get(None) #Probably dangerous to not specify a timeout
+	predscores = map( score, preds, [best_prey for _ in range(num_of_preds_per_gen)])
+	bestscore = max(predscores)
+	dindex = predscores.index(bestscore)
 
-	preyscores = preyResult.get(None)
-	score = max(preyscores)
-	index = preyscores.index(score)
-	best_prey = preyscores[index]
+#	preyscores = preyResult.get(None)
+	preyscores = map ( score, [best_pred for _ in range(len(preys))], preys )
+	bestscore = max(preyscores)
+	pindex = preyscores.index(bestscore)
 
+	best_prey = preys[pindex]
+	best_pred = preds[dindex]
+	
 if __name__ == "__main__":
-	gens = input("How many generations?")	
-	preds = input("How many preds per generations?")	
-	preys = input("How many preys per generations?")	
-	mutate(gens, preds, preys)
-	print(best_pred)
-	print(best_prey)
+    gens = input("How many generations?")	
+    preds = input("How many preds per generations?")	
+    preys = input("How many preys per generations?")	
+    mutate(gens, preds, preys)
+    print(best_pred)
+    print(best_prey)
