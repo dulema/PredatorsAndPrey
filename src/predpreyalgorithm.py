8fb505b6e25e1c24bfdc5c370d839239e6b01b63
sbadame <s.badame@gmail.com>
2010-04-20 18:31:55 -0400

    There is still a lingering freezing bug but HOLY CRAP IS MULTI-CORE AWESOME

--- src/predpreyalgorithm.py
+++ src/predpreyalgorithm.py
@@ -2,13 +2,13 @@
 import random
 from predpreymap import Map
 import copy
-#import multiprocessing
-#from multiprocessing import Pool
 from critter import Critter
 import critter
 
 best_pred = Critter(critter.PREDATOR)
 best_prey = Critter(critter.PREY)
+DEFAULT_SETTINGS = {"mapsize":20, "vegpercent":0.5, "preypercent":0.1, "predpercent":0.1, "sight":10}
+
 
 def reverse(direction):
     if direction == None or direction == 0:
@@ -39,7 +39,8 @@ def right(direction):
 #sensorydata holds the data from the map
 #move holds a direction like "Move towards predator"
 def directionConverter(sensorydata, move):
-    if sensorydata == (None, None, None, None, 0, 0):
+    #print(sensorydata)
+    if sensorydata == (None, None, None, None, 0, 0) or (None, None, None, None, None, None):
 	return random.randint(0, 6)
     toPred = sensorydata[1]
     toPrey = sensorydata[3]
@@ -52,105 +53,95 @@ def directionConverter(sensorydata, move):
      reverse(toPred), reverse(toPrey), reverse(toPlant),
      left(toPred),    left(toPrey),    left(toPlant),
      right(toPred),   right(toPrey),   right(toPlant), 0  ]
-    #print( "%s -> %s" % (move, allpossiblemoves[move]) )
     return allpossiblemoves[move]
 
-def score(x):
+def calcscore(x):
     pred = x[0]
     prey = x[1]
-    hooker = None
-    if len(x) > 2:
-	hooker = x[2]
 
-    world = Map(23, 0.5)
-    for p in pred.clone(4): world.setCritterAt(world.getRandomUntakenTile(), p)
-    for p in prey.clone(14): world.setCritterAt(world.getRandomUntakenTile(), p)
-    
+    settings = x[2] if len(x) > 2 else DEFAULT_SETTINGS
+    mapsize = settings["mapsize"] if "mapsize" in settings else 20
+    vegpercent = settings["vegpercent"] if "vegpercent" in settings else 0.5
+    preypercent = settings["preypercent"] if "preypercent" in settings else 0.1
+    predpercent = settings["predpercent"] if "predpercent" in settings else 0.1
+    maxhunger = settings["maxhunger"] if "maxhunger" in settings else 20
+    sight = settings["sight"] if "sight" in settings else 20
+
+    hooker = x[3] if len(x) > 3 else None
+
+    world = Map(mapsize, vegpercent)
+    for p in pred.clone(int((mapsize**2)*predpercent)): world.setCritterAt(world.getRandomUntakenTile(), p)
+    for p in prey.clone(int((mapsize**2)*preypercent)): world.setCritterAt(world.getRandomUntakenTile(), p)
+
     score = 0
     while len(world.getPreys()) >  0 and len(world.getPredators()) > 0:
-	print(len(world.getPreys()), len(world.getPredators()))
         #Score get incremented because it is just how every long the species lasts
 	score += 1
 
 	#Increment the hunger of all of the animals by 1
         for c in world.getCritters():
 		c.incrementStatus("hunger", 1)
-                if c.getStatus("hunger") >= 20:
+                if c.getStatus("hunger") >= maxhunger:
 			world.removeCritter(c)
 
-	print("About to find preds")
 	for c in world.getPredators():
 	    current_tile = world.getCritterXY(c)
-	    sensorydata = world.getSensoryData(c, 20)
-	    print(sensorydata)
+	    sensorydata = world.getSensoryData(c, sight)
 	    location = None
 	    move = None
 	    while True:
-		print("pred looping")
 		while location in (None, (-1, -1)):
-		    print("pred location loop")
                     critmove = c.getMove(sensorydata) 
-		    print(sensorydata, critmove)
 		    move = directionConverter(sensorydata,critmove ) 
 		    location = world.getCritterDest(c,move)
 		crit = world.getCritterAt(location)
 
 		if crit == c: #If it doesn't wanna move
-		    print("crit == c")
 		    break
 		elif crit == None:
 		    world.moveCritter(c, move)
-		    print("crit == None")
 		    break
 		elif crit.type == critter.PREY:
 		    world.removeCritter(crit)
 		    world.moveCritter(c, move)
 		    c.setStatus("hunger", 0)
-		    print("crit.type == critter.PREY")
 		    break
 		elif crit.type == critter.PREDATOR:
 		    location = None
 		else:
-		    print("pred WTFFF?")
+		    raise Exception("Unhandled case for preds")
 
 	    if hooker != None:
 		    hooker(world, score)
 
 	for c in world.getPreys():
 	    current_tile = world.getCritterXY(c)
-	    sensorydata = world.getSensoryData(c, 20)
+	    sensorydata = world.getSensoryData(c, sight)
 	    location = None
 	    while True:
-		print("prey looping")
 		while location in (None, (-1, -1) ): 
-		    print("prey location loop")
 		    critmove = c.getMove(sensorydata)
-		    print(sensorydata, critmove)
-		    move = directionConverter(sensorydata,critmove )
+		    move = directionConverter(sensorydata, critmove)
 		    location = world.getCritterDest(c, move)
 		crit = world.getCritterAt(location)
 
 		if crit == c: #if the prey decides not to move
 		    if world.isPlant(location):
 			c.setStatus("hunger", 0)
-		    print("crit == c")
 	            break
 		elif crit == None:
 		    world.moveCritter(c, move)
 		    if world.isPlant(location):
 			c.setStatus("hunger", 0)
-		    print("crit == None")
 		    break
 		elif crit.type == critter.PREY:
 		    location = None
-		    print("self")
 		elif crit.type == critter.PREDATOR:
 		    world.removeCritter(c)
 		    crit.setStatus("hunger", 0)
-		    print("crit.type == critter.PREDATOR")
 		    break
 		else:
-		    print("WTF?")
+		    raise Exception("Unhandled case for preys")
 
             if hooker != None:
 		    hooker(world, score)
@@ -182,42 +173,48 @@ def roundprogress(map, score):
     	#print("Progress at %s" % score)
     #	for critter in map.critters: print("%s at %s hunger:%s" % (critter.type, map.critters[critter], critter.getStatus("hunger")))
 
-def mutate(gens, num_of_preds_per_gen, num_of_prey_per_gen, progress=__printProgress): 
-    global best_pred, best_prey, score
-    #pool = Pool()
+
+def getCalcScoreArgs(preds, preys, bpred, bprey, settings):
+    bpreyclones = [copy.deepcopy(bprey) for _ in preds]
+    bpredclones = [copy.deepcopy(bpred) for _ in preys]
+    settingsclones = [settings for _ in range(max(len(preds),len(preys)))]
+    predArgs = zip(preds, bpreyclones, settingsclones[:len(preds)])
+    preyArgs = zip(bpredclones, preys, settingsclones[:len(preys)])
+    return predArgs, preyArgs
+
+def getResults(predArgs, preyArgs):
+    return map(calcscore, predArgs), map(calcscore, preyArgs) 
+
+def getMultiProcessedResults(predArgs, preyArgs):
+    #Now comes the multiprocessing magic...
+    import multiprocessing
+    from multiprocessing import Pool
+    pool = Pool()
+    predResults = pool.map_async(calcscore, predArgs)
+    preyResults = pool.map_async(calcscore, preyArgs)
+    return predResults.get(), preyResults.get()
+
+
+def mutate(gens, num_of_preds_per_gen, num_of_prey_per_gen, settings=DEFAULT_SETTINGS, progress=__printProgress): 
+    global best_pred, best_prey, calcscore
 
     for i in range(gens):
 	progress(i, gens)
 
-	preds = [pred for pred in best_pred.getMutations(num_of_preds_per_gen - 1)]
-	preds.append(best_pred)
+        preds = [pred for pred in best_pred.getMutations(num_of_preds_per_gen - 1)]
+        preds.append(best_pred)
 
-	preys =  [prey for prey in best_prey.getMutations(num_of_prey_per_gen - 1)]
-	preys.append(best_prey)
+        preys =  [prey for prey in best_prey.getMutations(num_of_prey_per_gen - 1)]
+        preys.append(best_prey)
 
-	#Generate a Pool of processes to run all of the scoring for the predators
-	#predResult = pool.map_async(score, zip(preds, [copy.deepcopy(best_prey) for _ in range(num_of_preds_per_gen)]) ) 
-	#preyResult = pool.map_async(score, zip([copy.deepcopy(best_pred) for _ in range(len(preys))], preys) )
-	
-	#Parse the results
-	#predscores = predResult.get(None) #Probably dangerous to not specify a timeout
-	predscores = []
-	for p in preds:
-		predscores.append(score((p, copy.deepcopy(best_prey), roundprogress)))
-        #predscores = map(score, zip(preds, [copy.deepcopy(best_prey) for _ in range(len(preds))], [roundprogress for _ in range(len(preds))] )   )
-	bestscore = max(predscores)
-	dindex = predscores.index(bestscore)
+	predArgs, preyArgs = getCalcScoreArgs(preds, preys, best_pred, best_prey, settings)
 
-	#preyscores = preyResult.get(None)
-	#preyscores = map(score, zip([copy.deepcopy(best_pred) for _ in range(len(preys))], preys) )
-	preyscores = []
-	for p in preys:
-		preyscores.append(score((copy.deepcopy(best_pred), p, roundprogress)))
-	bestscore = max(preyscores)
-	yindex = preyscores.index(bestscore)
+	predscores, preyscores = getMultiProcessedResults(predArgs, preyArgs)
+	#predscores, preyscores = getResults(predArgs, preyArgs)
 
-	best_prey = preys[yindex]
-	best_pred = preds[dindex]
+	best_prey = preds[predscores.index(max(predscores))]
+	best_pred = preys[preyscores.index(max(preyscores))]
+
 	
 if __name__ == "__main__":
     gens = input("How many generations?")	
