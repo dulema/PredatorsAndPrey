db80732d11ed511b06070033993a6961926e624a
sbadame <s.badame@gmail.com>
2010-04-24 15:19:25 -0400

    added a test case to mutate.py

--- src/mutate.py
+++ src/mutate.py
@@ -43,7 +43,6 @@ def mutatepdf( x ):
 
     newpdf = copy.deepcopy(pdf)
     increments = numpy.random.uniform(-increment, increment, pdfsize)
-    mutations = []
     for i in range(pdfsize):
         randominput = tuple(map(lambda x:numpy.random.random_integers(x), inputranges))
         if randominput not in newpdf:
@@ -54,6 +53,20 @@ def mutatepdf( x ):
         hist = newpdf[randominput] #Get the histogram for a move
         randommove = numpy.random.randint(0,len(hist)) #pick a random move to change
         hist[randommove] += increments[i] #Randomly modify the histogram
+        hist[randommove] = abs(hist[randommove])
         hist /= hist.sum() #Normalize the vector by dividing by the sum of the elements
-    mutations.append(newpdf)
-    return mutations
+    return newpdf
+
+
+
+if __name__ == "__main__":
+    pdf1 = {}
+    pdf2 = {}
+
+    result = createMutations( ((pdf1, 5), (pdf2, 3)), {"mutationincrement":0.3, "pdfpercent":0.5, "inputranges":[2, 3, 3] } )
+    for n, r in enumerate(result):
+        print(" == R %d of %d == "% (n, len(result)))
+        for i, x in enumerate(r):
+            print("\t == mutation %d ==" % (i))
+            for k,v in x.iteritems():
+                print("\t\t%s : %s" % (k, v) )
