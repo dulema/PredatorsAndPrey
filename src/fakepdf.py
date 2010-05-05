d7698c6c0568d81c4d5ca19c0d4a52c49f759b55
sbadame <s.badame@gmail.com>
2010-05-05 16:30:26 -0400

    removed print statement from fake

--- src/fakepdf.py
+++ src/fakepdf.py
@@ -6,7 +6,6 @@ histogram = numpy.array([0, 0, 0, 255, 0, 0, 0], dtype=numpy.uint8)
 allinputs = itertools.product(range(4), range(7),range(4), range(7),range(4), range(7), range(4))
 pdf = {}
 for input in allinputs:
-    print input
     pdf[input] = histogram.copy()
 print(histogram)
 pickle.dump(pdf, file("fixedpdf.pred", "w"))
